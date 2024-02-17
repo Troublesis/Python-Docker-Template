392a92b7d50277c29d557e5eb5cbdfb26c6f2a8f
Troublesis <14037176+troublesis@user.noreply.gitee.com>
2024-02-17 22:16:20 +1100

    add secret environment variable and develop watch in docker compose

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # python-docker
 
 A simple Python app for [Docker's Python Language Guide](https://docs.docker.com/language/python).
+
+# Instructions
+
+- Use command `rm -rf .git` to remove the git and add yours.
+- Run your project and use command `docker-compose up -d --build` to build and run the docker project.
+- Use command `docker-compose down` to shutdown the project
--- app/app.py
+++ app/app.py
@@ -1,6 +1,21 @@
+import os
+import time
+
 from util import config, config_list, log
 
-print("hello world!")
-log.info("This is an info message")
-print(config("DEFAULT", "TEST"))
-print(config_list("DEFAULT", "TEST_LIST"))
+
+def main():
+    print("hello world!")
+    log.info("This is an info message")
+    print(config("DEFAULT", "TEST"))
+    print(config_list("DEFAULT", "TEST_LIST"))
+
+    if "PASSWORD_FILE" in os.environ:
+        with open(os.environ["PASSWORD_FILE"], "r") as f:
+            password = f.read().strip()
+            print(f"Password: {password}")
+    time.sleep(2)
+
+
+while True:
+    main()
